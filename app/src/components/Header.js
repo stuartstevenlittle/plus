ee556a7fc26e7eed3f19cccc15fd85a599e228c4
Stuart <stuart@exelwines.co.uk>
2020-10-10 04:37:09 +0100

    end of Fri night

--- app/src/components/Header.js
+++ app/src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'gatsby'
 const Header = () => {
   return (
-    <nav className="bg-white py-8 border-b border-gray-200">
+    <nav className="bg-white py-6 border-b border-gray-200">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16">
           <div className="flex">
