1ee729bb7a88763f895836d28b109fed5d98f14e
Stuart <stuart@exelwines.co.uk>
2020-10-10 22:44:53 +0100

    make headings very black

--- app/src/components/Section.js
+++ app/src/components/Section.js
@@ -7,7 +7,7 @@ const Section = ({ title, children, colour, noShadow }) => {
     <section className={`bg-${colour}-100 ${noShadow ? '' : 'shadow'} w-full`}>
       <div className="max-w-screen-xl mx-auto pt-4 pb-6 px-4 sm:px-6 lg:px-8 xl:px-0">
         {title &&
-          <div className="section-header uppercase text-2xl sm:text-4xl text-center mb-2 text-gray-600">
+          <div className="section-header uppercase text-2xl sm:text-4xl text-center mb-2 text-gray-900">
             <h1 className="font-bold ">{firstWord}<span className="font-light ml-1">{theRest}</span></h1>
           </div>
         }
