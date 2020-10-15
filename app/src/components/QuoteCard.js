3a0f1dc462dce32a8b3247412046f3cb87906c1d
Stuart <stuart@exelwines.co.uk>
2020-10-15 21:55:30 +0100

    add styled quote marks

--- app/src/components/QuoteCard.js
+++ app/src/components/QuoteCard.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import Avatar from './Avatar'
+import LeftQuote from '../components/LeftQuote'
+import RightQuote from '../components/RightQuote'
 
 const QuoteCard = ({ quote }) => (
   <>
     { quote &&
-      <li key={quote.id} className="p-4 text-2xl overflow-hidden col-span-1 flex flex-col rounded shadow bg-white hover:shadow-lg transition ease-in-out duration-150">
-        <h3 >{quote.text}</h3>
+      <li key={quote.id} className="text-2xl p-4 min-h-64 overflow-hidden col-span-1 flex flex-col rounded shadow bg-white hover:shadow-lg transition ease-in-out duration-150">
+        <h3>
+          <LeftQuote />
+          <span className="italic"> {quote.text}</span>
+          <RightQuote />
+        </h3>
         <Avatar person={quote.quoteAuthor} />
       </li >}
   </>
