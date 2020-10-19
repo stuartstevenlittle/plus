eadc867af03cf8bec5b81812929bb01166fa63c4
Stuart <stuart@exelwines.co.uk>
2020-10-19 11:49:49 +0100

    add honeypot

--- app/src/components/ContactUs.js
+++ app/src/components/ContactUs.js
@@ -55,8 +55,9 @@ const ContactUs = () => {
         </div>
         <div className="bg-white py-16 px-4 sm:px-6 lg:col-span-3 lg:py-24 lg:px-8 xl:pl-12">
           <div className="max-w-lg mx-auto lg:max-w-none">
-            <form name="contact" method="POST" data-netlify="true" className="grid grid-cols-1 gap-y-6">
+            <form name="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field" className="grid grid-cols-1 gap-y-6">
               <input type="hidden" name="form-name" value="contact" />
+              <input className="hidden" name="bot-field" />
               <div>
                 <label htmlFor="name" className="sr-only">Name</label>
                 <div className="relative rounded-md shadow-sm">
