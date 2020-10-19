5b6ba7f221affa5d40a647a83fdea4fb4a7bf908
Stuart <stuart@exelwines.co.uk>
2020-10-19 11:24:49 +0100

    add netiflify forms

--- app/src/components/ContactUs.js
+++ app/src/components/ContactUs.js
@@ -56,6 +56,7 @@ const ContactUs = () => {
         <div className="bg-white py-16 px-4 sm:px-6 lg:col-span-3 lg:py-24 lg:px-8 xl:pl-12">
           <div className="max-w-lg mx-auto lg:max-w-none">
             <form name="contact" method="POST" data-netlify="true" className="grid grid-cols-1 gap-y-6">
+              <input type="hidden" name="form-name" value="contact" />
               <div>
                 <label htmlFor="full_name" className="sr-only">Name</label>
                 <div className="relative rounded-md shadow-sm">
